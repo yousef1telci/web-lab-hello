358a25ec75e03dbfd7b11bb41e3fcad440a61f5c
Yousef Taljbini <ywseft2006@gmail.com>
2026-02-26 21:32:21 +0300

    initial project setup with Vite React + TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad:[Yousef taljbini]</p>
+      <p>Ogrenci No: [230541601]</p>
+    </div>
+  )
+}
+
+export default App
